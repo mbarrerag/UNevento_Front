414ea9ec53686f4e352e9d0a526186c679a90cd8
MillerBarrera <mbarrerag@unal.edu.co>
2024-03-01 17:49:20 -0500

    ForgotPassword

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,14 +6,14 @@ import { NotFoundpageComponent } from './not-foundpage/not-foundpage.component';
 import { UserLoginComponent } from './UserLogin/user-login/user-login.component';
 import { SingUpUserComponent } from './Singup/sing-up-user/sing-up-user.component';
 import { HomeComponentComponent } from './Home/home-component/home-component.component';
-
+import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
 export const routes: Routes = [
 
 
   { path: 'singup', component: SingUpUserComponent }, 
   { path: 'login', component: UserLoginComponent }, 
   { path: 'home', component:HomeComponentComponent},
-  
+  { path: 'forgotpassword', component:ForgotPasswordComponent},
   { path: '', redirectTo: 'home',  pathMatch: 'full', },
   { path: '**', component: NotFoundpageComponent } 
   // { path:'dashboard',
--- src/app/forgot-password/forgot-password.component.ts
+++ src/app/forgot-password/forgot-password.component.ts
@@ -9,7 +9,20 @@ export class ForgotPasswordComponent {
   email: string = '';
   code: string = '';
   newPassword: string = '';
-
+ 
+ 
+ changeColor(event: MouseEvent) {
+  if ((event.target instanceof HTMLParagraphElement && event.target.classList.contains('subtittle'))  ) {
+    return;
+  }
+  const element = event.target as HTMLElement;
+    element.style.color = "#a1cb0b"; // Cambiar el color al hacer hover
+    element.removeEventListener('mouseover', this.changeColor); // Eliminar el event listener para evitar cambios posteriores
+  
+  
+   
+}
+  
   requestPasswordReset() {
     // Lógica para enviar una solicitud de restablecimiento de contraseña
     // Aquí puedes hacer una llamada a tu servicio o API correspondiente
