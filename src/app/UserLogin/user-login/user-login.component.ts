6d9d0383f6878a4400bcab59eb4223d02cbb9964
MillerBarrera <mbarrerag@unal.edu.co>
2024-03-06 16:06:16 -0500

    Images fixed

--- src/app/UserLogin/user-login/user-login.component.ts
+++ src/app/UserLogin/user-login/user-login.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-user-login',
   standalone: true,
-  imports: [],
+  imports: [RouterLink,RouterOutlet],
   templateUrl: './user-login.component.html',
   styleUrl: './user-login.component.css'
 })
