a89f007010e7a31608efbfacf49d8332f2e42b0b
MillerBarrera <mbarrerag@unal.edu.co>
2024-03-05 20:57:57 -0500

    Revert "todo lo nuevo 3"
    This reverts commit 7d6fce88a5c7d7ab3ec21ac32661663850b3de31.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,28 +1,36 @@
 
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { NotFoundpageComponent } from './not-foundpage/not-foundpage.component';
 import { UserLoginComponent } from './UserLogin/user-login/user-login.component';
 import { SingUpUserComponent } from './Singup/sing-up-user/sing-up-user.component';
 import { HomeComponentComponent } from './Home/home-component/home-component.component';
+import { MyeventsComponent } from './myevents/myevents.component'; 
+import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
+import { AboutUSComponent } from './about-us/about-us.component';
+import { ProfileComponent } from './User-components/profile/profile.component';
+import { EditProfileComponent } from './User-components/edit-profile/edit-profile.component';
+
 
 export const routes: Routes = [
 
 
   { path: 'singup', component: SingUpUserComponent }, 
   { path: 'login', component: UserLoginComponent }, 
+  { path: 'home', component: HomeComponentComponent},
+  { path: 'miseventos', component: MyeventsComponent },
   { path: 'home', component:HomeComponentComponent},
-  
+  { path: 'forgotpassword', component:ForgotPasswordComponent},
+  { path: 'aboutus', component:AboutUSComponent },
+  { path: 'profile', component:ProfileComponent},
+  { path: 'editprofile', component:EditProfileComponent},
   { path: '', redirectTo: 'home',  pathMatch: 'full', },
   { path: '**', component: NotFoundpageComponent } 
- 
   // { path:'dashboard',
   //   loadComponent: () => import(),
   //   children:[
-
   //   ]
-
   // },
 
 
