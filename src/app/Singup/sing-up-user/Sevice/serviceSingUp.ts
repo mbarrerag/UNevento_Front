2c2e7e85a6a7113bbde1fdec041e1cf5214df171
breinerLopez10 <breynerlopex234@gmail.com>
2024-03-21 21:22:55 -0500

    Nuevecito

---
+++ src/app/Singup/sing-up-user/Sevice/serviceSingUp.ts
@@ -0,0 +1,30 @@
+import { Injectable, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class serviceSingUp {
+  private baseUrl = 'http://localhost:8182/newuser'; // Reemplaza esto con la URL de tu backend
+
+
+
+  // constructor(private http: HttpClient) { }
+private http = inject(HttpClient)
+
+
+  // createUser(userData: any): Observable<any> {
+  //   return this.http.post<any>(this.baseUrl, userData);
+  // }
+
+
+  
+
+  createUser(userData: any) {
+  console.log(userData);
+  
+    return this.http.post<any>('http://localhost:8182/newuser', userData);
+  }
+}
+
--- src/app/Singup/sing-up-user/sing-up-user.component.ts
+++ src/app/Singup/sing-up-user/sing-up-user.component.ts
@@ -1,16 +1,83 @@
 import { Component } from '@angular/core';
-import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
+import { serviceSingUp } from './Sevice/serviceSingUp';
 
+
+
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms'; // Importa FormsModule aquí
+import { HttpClientModule } from '@angular/common/http';
 
 
 @Component({
   selector: 'app-sing-up-user',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './sing-up-user.component.html',
   styleUrl: './sing-up-user.component.css'
 })
+
 export class SingUpUserComponent {
+  userData = {
+    nombre: '',
+    apellido: '',
+    correo: '',
+    contrasena: ''
+  };
+
+  userData2 = {
+    "id": 0,
+    "nombre": "meluck",
+    "apellido": "b",
+    "email": "cv",
+    "contrasena": "meluck"
+  };
 
 
+
+
+  confirmPassword = '';
+  acceptTerms = false;
+  showConfirmPassword = false;
+  showPassword = false;
+  constructor(private serviceSingUp: serviceSingUp) {}
+
+  togglePasswordVisibility() {
+    this.showConfirmPassword = !this.showConfirmPassword;
 }
+
+togglePasswordVisibility2() {
+  this.showPassword = !this.showPassword;
+}
+
+
+  submitForm() {
+    // if (this.userData2.contrasena !== this.confirmPassword) {
+    //   console.error('Las contraseñas no coinciden');
+    //   return;
+    // }
+
+    // if (!this.acceptTerms) {
+    //   console.error('Debes aceptar los términos y condiciones');
+    //   return;
+    // }
+
+    // if (this.userData2.contrasena.length < 8) {
+    //   console.error('La contraseña debe tener al menos 8 caracteres');
+    //   return;
+    // }
+
+    this.serviceSingUp.createUser(this.userData2).subscribe(
+      response => {
+        console.log('Usuario creado exitosamente:', response);
+        // Aquí puedes manejar la respuesta del backend, como redireccionar al usuario a otra página, mostrar un mensaje de éxito, etc.
+      },
+      error => {
+        console.error('Error al crear usuario:', error);
+        // Aquí puedes manejar el error, como mostrar un mensaje de error al usuario, registrar el error, etc.
+      }
+    );
+  }
+}
+
+
