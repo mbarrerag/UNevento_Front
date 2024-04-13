a4489422426bcc9f46065a357cd27168d182ff13
nmart1nezl <nmartinezl@unal.edu.co>
2024-04-13 12:59:09 -0500

    Create Event Updated
    Se actualiza la petición de crear eventos tanto de facultad como no oficiales, además de que se agregan las validaciones de cada campo del formulario

--- src/app/User-components/create-community-events/create-community-events.component.ts
+++ src/app/User-components/create-community-events/create-community-events.component.ts
@@ -29,7 +29,7 @@ export class CreateCommunityEventsComponent {
     Facultad: string = 'No_Aplica';
     IdUsuario: number = parseInt(localStorage.getItem('id') || '0');
     token: string = localStorage.getItem('token') || '';
-    Aforo: number = 0;
+    Aforo: number = 1;
     Categoria: string = '';
     Descripcion: string = '';
     Imagen: any; // Initialize the "Imagen" property
@@ -71,6 +71,7 @@ export class CreateCommunityEventsComponent {
         lugar: this.Lugar,
         categoria: this.Categoria,
         Facultad: this.Facultad,
+        tipo:'NO_OFICIAL',
         fechaEvento: new Date(formattedFechaEvento),
         capacidad: this.Aforo,
         hora: this.Hora
