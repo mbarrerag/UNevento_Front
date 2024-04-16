a9d2524184a41a8e911ed4e083196b30a9c38eeb
nmart1nezl <nmartinezl@unal.edu.co>
2024-04-16 18:29:59 -0500

    Cancel Button - Modify Event
    Se agrega el Botón de Cancelación de modificación de evento en el respectivo modal

--- src/app/modal-modify-event/modal-modify-event.component.ts
+++ src/app/modal-modify-event/modal-modify-event.component.ts
@@ -26,6 +26,10 @@ export class ModalModifyEventComponent {
     location.reload();
   }
 
+  onCancel(){
+    this.closeModalModify.emit();
+    this.router.navigate(['/miseventos']);
+  }
 
 
   fechaValida: boolean = true; // Propiedad para controlar la validez de la fecha
