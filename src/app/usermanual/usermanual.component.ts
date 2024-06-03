c5e186e6be1594c2ea020248f7728263c59f080e
nmart1nezl <nmartinezl@unal.edu.co>
2024-06-02 20:34:25 -0500

    Navbar Setting
    Arreglo un problema del navbar cuando está en modo colapsado

--- src/app/usermanual/usermanual.component.ts
+++ src/app/usermanual/usermanual.component.ts
@@ -2,14 +2,16 @@ import { Component } from '@angular/core';
 import { NavbarAdminComponent } from '../commons/navbar-admin/navbar-admin.component';
 import { NavbarComponent } from '../commons/navbar/navbar.component';
 import { FooterComponent } from '../commons/footer/footer.component';
+import { NgIf } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-usermanual',
   standalone: true,
-  imports: [NavbarComponent,FooterComponent],
+  imports: [NavbarComponent, FooterComponent, NgIf, RouterLink],
   templateUrl: './usermanual.component.html',
   styleUrl: './usermanual.component.css'
 })
 export class UsermanualComponent {
-
+   seccionSeleccionada: string = '';
 }
