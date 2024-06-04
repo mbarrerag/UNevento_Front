6101a7b2fde9286d3ff8172cd54d10fa9281ac4d
nmart1nezl <nmartinezl@unal.edu.co>
2024-06-04 13:57:28 -0500

    UserManual Button disappears in usermanual page
    Modificación que hace que el manual de usuario se abra en otra pestaña, además de que hace desaparecer el botón para acceder al manual de usuario si nos encontramos en esa pestaña

--- src/app/commons/footer/footer.component.ts
+++ src/app/commons/footer/footer.component.ts
@@ -1,13 +1,24 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { Router } from '@angular/router';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink,NgIf],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
 export class FooterComponent {
 
+  constructor(private router: Router) { }
+
+  openUserManual() {
+    window.open('/usermanual', '_blank');
+  }
+
+  isUserManualRoute() {
+    return this.router.url === '/usermanual';
+  }
 }
