bbe067024fbae2198aa1f5ac2d7a039350d9b465
breinerLopez10 <breynerlopex234@gmail.com>
2024-06-05 21:57:04 -0500

    ultimo

--- src/app/Premium/premium/premium.component.ts
+++ src/app/Premium/premium/premium.component.ts
@@ -1,24 +1,24 @@
-
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { NavbarComponent } from '../../commons/navbar/navbar.component';
 import { FooterComponent } from '../../commons/footer/footer.component';
 import { AuthService } from '../../UserLogin/user-login/Services/auth/auth.service';
 import { Router } from '@angular/router';
-
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-premium',
   standalone: true,
-  imports: [FooterComponent, NavbarComponent],
+  imports: [FooterComponent, NavbarComponent, CommonModule],
   templateUrl: './premium.component.html',
-  styleUrl: './premium.component.css'
+  styleUrls: ['./premium.component.css']
 })
-
-
-
 export class PremiumComponent {
-  constructor(private http: HttpClient, private authService: AuthService, private router: Router) {}
+  constructor(
+    private http: HttpClient,
+    private authService: AuthService,
+    private router: Router
+  ) {}
 
   async realizarPago(id: number, precio: number) {
     if (this.authService.isLoggedIn()) { // Comprueba si el usuario está logueado
@@ -27,16 +27,17 @@ export class PremiumComponent {
       try {
         console.log('Realizando pago por plan UNevento...');
 
-        const response: any = await this.http.post(url, { id: id, title: "Pago por plan UNevento", unit_price: precio, quantity: 1 }).toPromise();
+        const response: any = await this.http.post(url, { id, title: "Pago por plan UNevento", unit_price: precio, quantity: 1 }).toPromise();
         
         console.log('Respuesta del backend:', response);
 
-        const id_preference = response.id;    
+        const id_preference = response.id;
         window.open(response.initPoint, '_blank');
       } catch (error) {
         console.error('Error al procesar el pago:', error);
       }
     } else {
+      console.log('Usuario no autenticado, redirigiendo a login...');
       this.router.navigate(['/login']); // Redirige al usuario a la página de login si no está logueado
     }
   }
