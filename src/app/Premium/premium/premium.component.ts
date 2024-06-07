2f1f4a2e7e99eb77350711aa8be0cc0a016713f3
nmart1nezl <nmartinezl@unal.edu.co>
2024-06-06 20:46:22 -0500

    Arreglo Premium

--- src/app/Premium/premium/premium.component.ts
+++ src/app/Premium/premium/premium.component.ts
@@ -1,33 +1,31 @@
+
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { NavbarComponent } from '../../commons/navbar/navbar.component';
 import { FooterComponent } from '../../commons/footer/footer.component';
 import { AuthService } from '../../UserLogin/user-login/Services/auth/auth.service';
 import { Router } from '@angular/router';
-import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-premium',
   standalone: true,
-  imports: [FooterComponent, NavbarComponent, CommonModule],
+  imports: [FooterComponent, NavbarComponent],
   templateUrl: './premium.component.html',
-  styleUrls: ['./premium.component.css']
+  styleUrl: './premium.component.css'
 })
+
+
+
 export class PremiumComponent {
-  constructor(
-    private http: HttpClient,
-    private authService: AuthService,
-    private router: Router
-  ) {}
+constructor(private http: HttpClient, private authService: AuthService, private Router: Router){}
+
+
 
   async realizarPago(id: number, precio: number) {
-    if (this.authService.isLoggedIn()) { // Comprueba si el usuario está logueado
-      const url = "https://uneventoback-production-3c28.up.railway.app/create-preference";
-
 
   const url = "https://uneventoback-production-3c28.up.railway.app/create-preference";
   if (this.authService.isLoggedIn()) { // Corrige el acceso a isLoggedIn()
-    const url = "https://uneventoback-production-3c28.up.railway.app/create-preference";
 
 
     try {
@@ -41,10 +39,6 @@ export class PremiumComponent {
       window.open(response.initPoint, '_blank');
 
 
-        const response: any = await this.http.post(url, { id, title: "Pago por plan UNevento", unit_price: precio, quantity: 1 }).toPromise();
-        
-        console.log('Respuesta del backend:', response);
-
 
 
     // const url2 = "https://uneventoback-production-3c28.up.railway.app/Succes-update";
@@ -56,16 +50,13 @@ export class PremiumComponent {
     } catch (error) {
       console.error('Error al procesar el pago:', error);
     }
-
-        const id_preference = response.id;
-        window.open(response.initPoint, '_blank');
-      } catch (error) {
-        console.error('Error al procesar el pago:', error);
-      }
-
     } else {
-      console.log('Usuario no autenticado, redirigiendo a login...');
-      this.router.navigate(['/login']); // Redirige al usuario a la página de login si no está logueado
+      this.Router.navigate(['/login']);
     }
   }
+
+
 }
+
+
+  
